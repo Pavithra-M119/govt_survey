b5a4000c9c24bba4cdbc2786c8dd5d554c45a4ce
Pavithra-M119 <pavithramuniyandi07@gmail.com>
2023-08-22 11:32:02 +0530

    altered commit

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,21 @@ public class Main {
         String name;
         int age;
         double salary;
-        // Create a web form that people can use to submit their information.
+        // information of people
         System.out.println("enter your name:");
         name = input.next();
         System.out.println("enter your age:");
         age = input.nextInt();
         System.out.println("enter your salary:");
         salary = input.nextDouble();
-        // Validate the input to make sure that no negative values are being accepted for age and salary.
+        // checking for negative values
         if (age < 0 || salary < 0) {
             System.out.println("Error: Negative values are not allowed.");
             return;
+        }
+        else
+        {
+            System.out.println("Citizens informations are: "+name+" "+age+" "+salary);
         }
     }
 }
